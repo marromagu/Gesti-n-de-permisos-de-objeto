9a39c7c8f875ad6d88362270ef03b635398b1901
marromagu <mromagu2611@g.educaand.es>
2023-03-14 20:19:03 +0100

    first commit

---
+++ GestionPermisosObjeto.sql
@@ -0,0 +1,27 @@
+/*1.	Conéctate como system y otorga privilegios al empleado1 para seleccionar, insertar, borrar y
+ modificar datos de la tabla jugadores del usuario nbaXX de forma que pueda conceder este permiso a otros usuarios.*/
+ 
+/*2.	Consulta los privilegios sobre objetos asignados al empleado1 (dba_tab_privs).*/
+
+/*3.	Conéctate como empleado1 y concede los privilegios seleccionar, insertar, borrar y
+ modificar datos de la tabla jugadores del usuario nbaXX al empleado2 ¿Es posible? ¿Por qué?*/
+
+/*4.	Como empleado1 consulta los privilegios que ha concedido y a quien. Explica qué es cada columna (all_tab_privs_made).*/
+
+/*5.	Como empleado1 consulta los privilegios que tiene concedidos (recibidos) y quién se los ha concedido. Explica qué es cada columna (all_tab_privs_recd).*/
+
+/*6.	Conéctate como system y consulta los privilegios de sistema del empleado2.*/
+
+/*7.	Observa quien se los ha concedido (grantor).*/
+
+/*8.	Consulta los privilegios de sistema que ha concedido el empleado1.*/
+
+/*9.	Como empleado2 consulta los privilegios que ha concedido y a quien.*/
+
+/*10.	Como empleado2 consulta los privilegios que tiene concedidos (recibidos) y quien se los ha concedido.*/
+
+/*11.	Desde system revoca al empleado1 los privilegios para seleccionar, insertar, borrar y modificar datos de la tabla coches del usuario taller.
+ ¿Se le ha revocado el permiso de también al empleado2? ¿Por qué?*/
+
+/*12.	Borra los usuarios empleado1, empleado 2 y empleado3 obligando a que se borren todos los objetos de sus esquemas.
+ Recuerda que previamente debes cerrar las sesiones que estos usuarios tuvieran abiertas.*/
